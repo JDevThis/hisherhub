6641ae8cf35ef89d4bbe3f8140133673d661b0c5
JDevThis <152988655+JDevThis@users.noreply.github.com>
2024-01-21 20:16:17 +0000

    Register page

---
+++ app/register/page.tsx
@@ -0,0 +1,22 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import RegisterForm from '@/app/ui/reg-form';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col">
+
+      <div className=" flex grow justify-center flex-col gap-4 md:flex-row">
+        <div className="flex justify-center gap-6 rounded-lg bg-gray-50">
+          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}> </p>
+          <div>
+            { <RegisterForm /> }
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
